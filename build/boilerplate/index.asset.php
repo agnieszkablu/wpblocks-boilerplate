3fb90ca00181af267901170f54dbb156ce3347ea
Agnieszka Sikora <sikora.au@gmail.com>
2026-02-26 17:22:52 +0100

    formatting

--- build/boilerplate/index.asset.php
+++ build/boilerplate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'aca00ead400a664480c1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3129be560e2ea9741172');
